6bc1a257e60e942a829060df56cd34004eaa89bc
unknown <saynegrojas@yahoo.com>
2019-05-11 20:15:21 -0700

    homework 3-Psychic game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,60 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10; //start off with 9 guesses till game over
+var guessesSoFar = [];
 
-// //variables that hold wins, losses, left guesses, guesses so far and set them equal to their respective value
-// var wins = 0,
-// losses = 0,
-// guessesLeft = 9. //start off with 9 guesses till game over
-// guessesSoFar = 0;
+//array of letters
+var wordsToGuess = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z'];
 
-// //array of letters
-//  var wordsToGuess = ['k','i','n','g','s','l','a','y','e','r'];
+//reference to id
+var directionsText = document.getElementById("directions-text");
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessText = document.getElementById("guess-text");
+var guessAmountText = document.getElementById("guess-amount-text");
 
-//     var directionsText = document.getElementById("directions-text");
-//     var winsText = document.getElementById("wins-text");
-//     var lossesText = document.getElementById("losses-text");
-//     var guessText = document.getElementById("guess-text");
-//     var guessAmountText = document.getElementById("guess-amount-text");
+//randomly pic a letter
+var randomLetterSelect = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
 
-//  var randomGuess = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
+//function runs when the user presses a key.
+document.onkeyup = function (event) {
+    var userGuess = event.key;
 
 
-// //console.log(randomGuess);
+    //*****GAME INSTRUCTIONS*****
+    //user has to guess which letter the program chose (randomly)
+    //guesses so far (specific letters that the user typed. Display)
+    //wins(# of times the user guessed the letter correctly)
+    //guess left (# of guess left) (update)
+    //losses(# of times the user guessed the letter inccorrectly)
+    //when the player wins, increase wins counter & start over without refresh
+    //when the player loses, increase wins counter & start over without refresh
+    console.log(randomLetterSelect);
+    console.log(guessesSoFar)
+    if (userGuess === randomLetterSelect) {
+        wins++;
+        guessesLeft = 10;
+        guessesSoFar = [];
+        //when player wins, guessesLeft is set back to 10, guessesSoFar is set to an empty array
+        //when guessesSoFar = [], set randomLetterSelect generates another random number
+        if(guessesSoFar = []){
+            randomLetterSelect = wordsToGuess[Math.floor(Math.random() * wordsToGuess.length)];
+            //error: leaves empty until key is pressed
+        }
+    } else if (userGuess != randomLetterSelect) {
+        guessesLeft--;
+    }
+     if (guessesLeft === 0) {
+        losses++;
+        guessesLeft = 10;
+        guessesSoFar = [];
+    }
+    guessesSoFar += userGuess;
 
-// // This function is run whenever the user presses a key.
-// document.onkeyup = function(event) {
-
-//     // Determines which key was pressed.
-//     var userGuess = event.key;
-
-//     if (userGuess === randomGuess) {
-//           wins++;
-//         } else if (userGuess != randomGuess) {
-//           losses++;
-//           guessesLeft--;
-//         }
-
-//         //Display game content 
-//         winsText.textContent = "Wins: " + wins;
-//         lossesText.textContent = "Losses: " + losses;
-//         guessText.textContent = "Guesses Left " + guessesLeft;
-//         guessAmountText.contains = "Guesses so Far " + guessesSoFar;
-
-//         // computerChoiceText.textContent = "The computer chose: " + computerGuess;
-//         // winsText.textContent = "wins: " + wins;
-//         // lossesText.textContent = "losses: " + losses;
-//         // tiesText.textContent = "ties: " + ties;
-// }
-// /* <p id="directions-text">
-    //         <strong>Guess what letter I'm thinking of</strong>    
-    //     </p>
-    //     <p id="wins-text">
-    //         <strong>Wins:</strong>
-    //     </p>
-    //     <p id="losses-text">
-    //         <strong>Losses:</strong>
-    //     </p>
-    //     <p id="guess-text">
-    //         <strong>Guesses Left:</strong>
-    //     </p>
-    //     <p id="guess-amount-text">
-    //         <strong>Guesses so Far:</strong>
-    //     </p>
-    // </div> */
+    //Display game content 
+    winsText.textContent = "Wins: " + wins;
+    lossesText.textContent = "Losses: " + losses;
+    guessText.textContent = "Guesses Left: " + guessesLeft;
+    guessAmountText.textContent = "Guesses so Far: " + guessesSoFar;
+    //variables that hold wins, losses, left guesses, guesses so far and set them equal to their respective value
+}
